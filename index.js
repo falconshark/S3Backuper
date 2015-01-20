0f289bf07c69ea19802cda47bffae5061e3e7d6b
Sardo Ip <sardo.ip@nxtomo.hk>
2015-01-20 17:45:30 +0800

    Can get images file from S3

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 //Import modules
 var AWS = require('aws-sdk');
-var async = require('async');
 var util = require('util');
 var imgur = require('imgur-node-api');
 var path = require('path');
@@ -15,6 +14,7 @@ exports.handler = function(event, context) {
     
     for (i = 0; i < event.Records.length; i++){
 
+        var srcBucket = event.Records[i].s3.bucket.name;
         var srcKey = event.Records[i].s3.object.key;
 
         console.log("The file which you updated is " + srcKey);
@@ -29,12 +29,31 @@ exports.handler = function(event, context) {
 
         var imageType = typeMatch[1];
 
-        if(imageType != "jpg" && imageType ! ="png"){
+        if(imageType != "jpg" && imageType !="png"){
 
             console.log('Skipping non-image ' + srcKey);
 
             return;
         }
+
+        //Download images from S3
+
+        s3.getObject({
+            Bucket: srcBucket,
+            Key: srcKey
+        },upload);
+
+        function upload(err,images){
+
+            if(err){
+                console.log(err);
+                return;
+            }
+
+            console.log(images);
+
+            context.done();
+        } 
     }
 }
 
